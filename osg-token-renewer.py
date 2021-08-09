23801a4bed80d03383bd84aab9bacbf84ffdc932
Carl Edquist <edquist@cs.wisc.edu>
2021-08-09 13:05:53 -0500

    initial osg-token-renewer script (SOFTWARE-4719)
    Perhaps functional, but still WIP

---
+++ osg-token-renewer.py
@@ -0,0 +1,56 @@
+#!/usr/bin/env python3
+
+import configparser
+import subprocess
+
+CONFIG_PATH = '/etc/osg/tokens/renewer.ini'
+
+#As root, the user runs oidc-gen -w device ACCOUNT_SHORTNAME
+
+# oidc-gen -w device ClientNameo
+# - misc prompts, including password, which can be got with --pw-cmd=CMD
+
+# oidc-token --aud="<SERVER AUDIENCE>" <CLIENT NAME>
+
+
+def make_all_tokens(config):
+    tokens = [ sec for sec in config.sections() if sec.startswith("token ") ]
+
+    for t in tokens:
+        print(t)
+        for k,v in config[t].items():
+            print("{}: {}".format(k,v))
+        print("---")
+        mktoken(config[t])
+        print("===")
+
+
+def option_if(name, val):
+    return ['--{}={}'.format(name, val)] if val else []
+
+
+def mktoken(cfg):
+    #oidc-token --aud="<SERVER AUDIENCE>" <CLIENT NAME> > <TOKEN PATH>
+    prog    = ['oidc-token']
+    aud     = option_if('aud',   cfg.get("audience")    )
+    scope   = option_if('scope', cfg.get("scope")       )
+    time    = option_if('time',  cfg.get("min_lifetime"))
+    account = [cfg.get("account")]
+    cmdline = prog + aud + scope + time + account
+    dest    = cfg.get("token_path")
+    print(cmdline)
+    token_blob = subprocess.check_output(cmdline)
+    print("> %s" % dest)
+    with open(dest, "wb") as w:
+        w.write(token_blob)
+
+
+def main():
+    config = configparser.ConfigParser()
+    config.read(CONFIG_PATH)
+    make_all_tokens(config)
+
+
+if __name__ == '__main__':
+    make_all_tokens()
+
---
+++ rpm/osg-token-renewer.spec
@@ -0,0 +1,32 @@
+Name:      osg-token-renewer
+Summary:   oidc-agent token renewal service and timer
+Version:   0.1
+Release:   0%{?dist}
+License:   ASL 2.0
+URL:       http://www.opensciencegrid.org
+
+Requires: oidc-agent
+
+
+%description
+%summary
+
+%build
+
+%install
+install -d $RPM_BUILD_ROOT/%{_bindir}
+install -d $RPM_BUILD_ROOT/%{_sysconfdir}/osg/tokens
+install -m 755 %{name}.py $RPM_BUILD_ROOT/%{_bindir}/%{name}
+install -m 600 renewer.ini $RPM_BUILD_ROOT/%{_sysconfdir}/osg/tokens
+
+
+%files
+%defattr(-,root,root,-)
+%{_bindir}/%{name}
+
+%config(noreplace) %{_sysconfdir}/osg/tokens/renewer.ini
+
+%changelog
+* Thu Aug 05 2021 Carl Edquist <edquist@cs.wisc.edu> - 0.1-1
+- Initial release
+
