2030a8f867bd3333ea09f01632e69f6cd0f92e7d
Carl Edquist <edquist@cs.wisc.edu>
2021-08-16 09:44:14 -0500

    add accounts with oidc-add (SOFTWARE-4719)

--- osg-token-renewer.py
+++ osg-token-renewer.py
@@ -16,6 +16,19 @@ OIDC_SOCK   = '/var/run/osg-token-renewer/oidc-agent'
 # oidc-token --aud="<SERVER AUDIENCE>" <CLIENT NAME>
 
 
+def add_all_accounts(config):
+    accts = [ sec for sec in config.sections() if sec.startswith("account ") ]
+
+    for acct in accts:
+        print(acct)
+        if 'password_file' not in config[acct]:
+            print("missing 'password_file' attr; skipping %s" % acct)
+            continue
+        shortname = acct.split()[1]
+        pwfile = config[acct]['password_file']
+        add_account(shortname, pwfile)
+
+
 def make_all_tokens(config):
     tokens = [ sec for sec in config.sections() if sec.startswith("token ") ]
 
@@ -52,12 +65,20 @@ def mktoken(cfg):
               file=sys.stderr)
 
 
+def add_account(acct, pwfile):
+    pwcmd = "cat '%s'" % pwfile.replace("'", r"'\''")
+    cmd = ["oidc-add", "--pw-cmd=%s" % pwcmd, acct]
+    out = subprocess.check_output(cmd)
+    print("# oidc-add ... %s (%s)" % (acct, out))
+
+
 def main():
     config = configparser.ConfigParser()
     config_path = os.environ.get("OSG_TOKEN_RENEWER_CONFIG_PATH", CONFIG_PATH)
     config.read(config_path)
     if 'OIDC_SOCK' not in os.environ:
         os.environ['OIDC_SOCK'] = OIDC_SOCK
+    add_all_accounts(config)
     make_all_tokens(config)
 
 
