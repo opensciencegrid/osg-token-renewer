ff05e23c2985a6f8c31d5e9e0b5ba02c04afb2a8
Carl Edquist <edquist@cs.wisc.edu>
2021-08-17 11:26:39 -0500

    pass pw-file name with explicit option (SOFTWARE-4719)

--- osg-token-renewer-setup.sh
+++ osg-token-renewer-setup.sh
@@ -4,15 +4,29 @@ set -e
 fail () { echo "$@" >&2; exit 1; }
 
 usage () {
-  echo "usage: $(basename "$0") CLIENT_NAME scopes..."
+  echo "usage: $(basename "$0") [options] CLIENT_NAME scopes..."
   echo
   echo "   eg: $(basename "$0") myclient123 wlcg offline_access"
   echo
+  echo "Options:"
+  echo "  --pw-file /path/to/pwfile"
   exit 1
 }
 
-[[ $1 = -* ]] && usage
+while [[ $1 = -* ]]; do
+case $1 in
+  --pw-file ) pwfile=$2; shift 2 ;;
+   -*       ) usage ;;
+esac
+done
+
 [[ $2 ]] || usage
+
+issuer=https://wlcg.cloud.cnaf.infn.it/
+client_name=$1
+shift
+scopes=$*
+[[ $pwfile ]] || pwfile=/etc/osg/tokens/$client_name.pw
 
 cleanup () {
   oidc-agent -k >/dev/null
@@ -22,18 +36,13 @@ cleanup () {
   fi
 }
 
-issuer=https://wlcg.cloud.cnaf.infn.it/
-client_name=$1
-shift
-scopes=$*
-pwfile=/etc/osg/tokens/$client_name.pw
-
-[[ -e $pwfile || -r /dev/fd/9 ]] ||
+[[ -e $pwfile ]] ||
 fail "please create /etc/osg/tokens/$client_name.pw with encryption password"
 
 if [[ $UID = 0 ]]; then
   # open $pwfile as root, then re-run this script under service account
-  exec su osg-token-svc -s /bin/bash -c '"$@"' -- - "$0" "$client_name" "$@"
+  exec su osg-token-svc -s /bin/bash -c '"$@"' -- - \
+  "$0" --pw-file /dev/fd/9 "$client_name" "$@"
 fi 9<"$pwfile"
 
 eval $(oidc-agent)
@@ -41,7 +50,7 @@ trap cleanup EXIT
 
 ( echo "$issuer"
   echo "$scopes"
-) | oidc-gen -w device --pw-cmd="cat /dev/fd/9" "$client_name"
+) | oidc-gen -w device --pw-cmd="cat '$pwfile'" "$client_name"
 
 echo
 echo
