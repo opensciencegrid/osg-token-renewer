5295a1c45b48c57b0f717dd55bef97e096b85e1c
Carl Edquist <edquist@cs.wisc.edu>
2022-04-07 17:48:49 -0500

    fix permissions handling for pwfile in sub-process
    The "cat /dev/fd/9" implies re-opening the pwfile (in linux), which is
    subject to the permissions check. This probably only ever worked before
    if pwfile was root/644 or osg-token-svc/600.
    
    Fix by using a dup instead:
    
      # ls -l testfile
      -rw------- 1 root root 7 Apr  7 17:24 testfile
      # cat testfile
      winner
      # exec 9< testfile
      # su - user
      $ cat /dev/fd/9
      cat: /dev/fd/9: No such file or directory
      $ cat < /dev/fd/9
      -su: /dev/fd/9: Permission denied
      $ cat <&9
      winner
    
    In order to handle this sensibly in the script, add a --pw-fd option,
    for internal use only, as --pw-file /dev/fd/9 is fundamentally broken
    since it can't be opened by the (non-root) service account.

--- osg-token-renewer-setup.sh
+++ osg-token-renewer-setup.sh
@@ -13,11 +13,14 @@ usage () {
   echo "  --manual                     Manual client registration"
   exit 1
 }
+
 pwfile=
+pwfd=
 manual=
 while [[ $1 = -* ]]; do
 case $1 in
   --pw-file ) pwfile=$2; shift 2 ;;
+  --pw-fd   ) pwfd=$2;   shift 2 ;;  # internal option only
   --manual  ) manual='--manual'; shift 1 ;;
    -*       ) usage ;;
 esac
@@ -43,13 +46,13 @@ fail "please create /etc/osg/tokens/$client_name.pw with encryption password"
 if [[ $UID = 0 ]]; then
   # open $pwfile as root, then re-run this script under service account
   exec su osg-token-svc -s /bin/bash -c '"$@"' -- - \
-  "$0" $manual --pw-file /dev/fd/9 "$client_name"
+  "$0" $manual --pw-fd 9 "$client_name"
 fi 9<"$pwfile"
 
 eval $(oidc-agent)
 trap cleanup EXIT
 
-oidc-gen -w device $manual --pw-cmd="cat '$pwfile'" "$client_name"
+oidc-gen -w device $manual --pw-cmd="cat <&$pwfd" "$client_name"
 
 echo
 echo
